daa7ec822f80f9f7ee7cd1e08ac7dac0e7b4f15f
madMonkei <alexis.j.dubois@gmail.com>
2016-05-13 17:59:56 +0200

    Add files via upload

--- portailMemoire/script/portail.js
+++ portailMemoire/script/portail.js
@@ -13,11 +13,13 @@ function addYearToPromotion(from,how_many)
 	how_many = how_many!=undefined && !isNaN(how_many) ? how_many : 9; 
 	var promos = $('#promotion');
 	var start_year;
-	if(from==undefined && !isNaN(from))
+	console.log(from);
+	console.log(how_many);
+	if(from==undefined || isNaN(from))
 	{
 		console.log(promos);
-		console.log($(promos).children('option'));
-		start_year = parseInt($(promos).children('option[value!=""]').value());
+		console.log($(promos).children('option[value!=""]'));
+		start_year = parseInt($(promos).children('option[value!=""]').val());
 	}
 	else
 	{
@@ -25,17 +27,24 @@ function addYearToPromotion(from,how_many)
 	}
 	console.log(start_year);
 	var pre_option = $(promos).children('option[value="'+start_year+'"]');
+
 	var pre_year = start_year;
 	for(var i=0;i<how_many;i++)
 	{
+		console.log(i);
 		var next_option = $(pre_option).next();
 		var next_year = pre_year+1;
-		if(next_option.length>0 && !$(next_option).value()==next_year)
+		console.log(next_option);
+		console.log(next_year);
+		console.log(pre_option);
+		if(next_option.length==0 || $(next_option).val()!=next_year)
 		{
 			var new_option = $('<option></option')
 				.attr('value',next_year)
 				.text(next_year);
-			$(pre_option).appendAfter(next_option);
+			console.log(pre_option);
+			$(pre_option).after(new_option);
+			next_option=new_option;
 		}
 		pre_year=next_year;
 		pre_option=next_option;
